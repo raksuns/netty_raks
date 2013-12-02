e26c89acbe9c0220220a6ad484a9f093713e5e91
raksuns <raksuns@gmail.com>
2013-12-02 20:39:41 +0900

    Add discard example client.

--- netty-child/src/main/java/io/netty/child/discard/DiscardClient.java
+++ netty-child/src/main/java/io/netty/child/discard/DiscardClient.java
@@ -1,5 +1,24 @@
 package io.netty.child.discard;
 
+/**
+ * Keeps sending random data to the specified address.
+ */
 public class DiscardClient {
-
+	private final String host;
+	private final int port;
+	private final int firstMessageSize;
+	
+	public DiscardClient(String host, int port, int firstMessageSize) {
+		this.host = host;
+		this.port = port;
+		this.firstMessageSize = firstMessageSize;
+	}
+	
+	public void run() throws Exception {
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+	}
 }
